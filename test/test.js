9971dd64f8ffee6f7ccb1be6fa04d78ba4149c3c
Sunday Okpokor <sundayokpokor@gmail.com>
2018-11-09 19:28:35 +0100

    simple node app with test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', function(){
+    it('should respond with Hello World!',  function(done){
+        request(app).get('/').expect('Hello World!', done);
+    });
+});
