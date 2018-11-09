bc47955235d2f0dd2c28991025c79e7d3d3a0797
Sunday Okpokor <sundayokpokor@gmail.com>
2018-11-09 22:02:43 +0100

    test update

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', function(){
-    it('should respond with Hello World!',  function(done){
+    it('should respond with Hello World!!!',  function(done){
         request(app).get('/').expect('Hello World!!!', done);
     });
 });
