571ebc1beea9ad65fddeb68d81f9515c1b087e01
Sunday Okpokor <sundayokpokor@gmail.com>
2018-11-09 21:49:43 +0100

    index change

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require('../index');
 
 describe('GET /', function(){
     it('should respond with Hello World!',  function(done){
-        request(app).get('/').expect('Hello World!', done);
+        request(app).get('/').expect('Hello World!!!', done);
     });
 });
