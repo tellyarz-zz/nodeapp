fe3e7f389a78883ee87acf2480d7afcc7b820b8c
Sunday Okpokor <sundayokpokor@gmail.com>
2018-11-10 01:08:28 +0100

    update output

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', function(){
-    it('should respond with Hello Jenkins CI/CD!',  function(done){
-        request(app).get('/').expect('Hello Jenkins CI/CD!', done);
+    it('should respond with Hello Jenkins CI/CD',  function(done){
+        request(app).get('/').expect('Hello Jenkins CI/CD', done);
     });
 });
